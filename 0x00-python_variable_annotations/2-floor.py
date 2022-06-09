67cb810c9d1107f919607f76a907c49784d59ae0
paulacarvani <3312@holbertonschool.com>
2022-06-09 16:20:02 -0500

    Task 2. Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - floor
+"""
+
+
+def floor(n: float) -> int:
+    """
+        Args:
+            n: float number
+        Return:
+            Float number truncated to floor
+    """
+
+    return int(n)
